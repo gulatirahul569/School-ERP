f6aad2e07e30e93dc663016610a90537b564fc30
gulatirahul569 <rahulgulati425@gmail.com>
2026-07-17 18:11:19 +0530

    deployment started

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import API from "../services/API";
+import API from "../services/api";
 
 const AuthContext = createContext();
 
